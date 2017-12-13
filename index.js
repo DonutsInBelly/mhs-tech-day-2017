a254d9bff7c12d065203156030d37db899f4feb5
Carlin Au <carlinau@gmail.com>
2017-12-13 07:31:32 -0500

    minor change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var client = new twilio(config.sid, config.token);
 app.use(bodyParser.json());
 
 app.post('/resources', function(request, response) {
-  console.log(response.body.toString('utf8'));
+  console.log(request.body);
 });
 
 app.listen(process.env.PORT || 8080);
