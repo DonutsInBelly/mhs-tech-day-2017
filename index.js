82cdec23ffcae6e2648d83ceb1b1ddbb5a32bef3
Carlin Au <carlinau@gmail.com>
2017-12-13 07:31:18 -0500

    bodyparser

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var twilio = require('twilio');
 var config = require('./config.js');
 
 var app = express();
 var client = new twilio(config.sid, config.token);
+
+app.use(bodyParser.json());
 
 app.post('/resources', function(request, response) {
   console.log(response.body.toString('utf8'));
