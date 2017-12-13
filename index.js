209c20191d25787e7383a0393037f551dd827974
Carlin Au <carlinau@gmail.com>
2017-12-13 07:37:13 -0500

    urlencoded not json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var config = require('./config.js');
 var app = express();
 var client = new twilio(config.sid, config.token);
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/resources', function(request, response) {
   console.log(request.body);
