219616eb4b7a125894719bad62a6a6b019e77a64
Carlin Au <carlinau@gmail.com>
2017-12-13 07:57:44 -0500

    updated url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/resources', function(request, response) {
   console.log(request.body);
-  response.send('<Response><Message>https://github.com/dominusbelli/intro-to-nodejs/tree/master/MHS-12-13</Message></Response>');
+  response.send('<Response><Message>https://github.com/dominusbelli/mhs-tech-day-2017</Message></Response>');
 });
 
 app.listen(process.env.PORT || 8080);
